c07a2c978e2ad58838e8dbb1d91aa12c5379cbb1
Kees Postma <kk.postma@gmail.com>
2017-05-25 17:18:56 +0100

    Completes MVP functionality and basic views

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,11 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
@@ -12,5 +13,21 @@ class CocktailsController < ApplicationController
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end

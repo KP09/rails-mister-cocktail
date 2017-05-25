f98513eb7efc74f99726568ba96670ca37acf565
Kees Postma <kk.postma@gmail.com>
2017-05-25 16:07:13 +0100

    Completes basic cocktails#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Dose.destroy_all
+Cocktail.destroy_all
 Ingredient.destroy_all
 
 url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
@@ -6,4 +7,16 @@ url_hash = JSON.parse(open(url) { |io| io.read })
 
 url_hash["drinks"].each do |element|
   Ingredient.create!(name: element["strIngredient1"])
+end
+
+COCKTAILS = ["Mojito", "Vodka Martini", "Espresso Martini", "Reggae Rum Punch"]
+
+COCKTAILS.each do |cocktail|
+  Cocktail.create!(name: cocktail)
+end
+
+5.times do
+  cocktail = Cocktail.all.sample
+  ingredient = Ingredient.all.sample
+  Dose.create!(description: "Some description", cocktail: cocktail, ingredient: ingredient)
 end

35c4c8d073b2953b95fc6615b70b52a94e7c27ff
Kees Postma <kk.postma@gmail.com>
2017-05-25 14:46:56 +0100

    Adds JSON gem and builds seed file to populate ingredients

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+gem 'json'
 
 
 # Use CoffeeScript for .coffee assets and views
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Ingredient.destroy_all
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+url_hash = JSON.parse(open(url) { |io| io.read })
+
+url_hash["drinks"].each do |element|
+  Ingredient.create!(name: element["strIngredient1"])
+end

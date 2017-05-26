7dc294909dedf6572cd3083a65c8a4643afa7eb0
Kees Postma <kk.postma@gmail.com>
2017-05-26 11:41:30 +0100

    Allows user to upload an image to a cocktail. Specifies Activerecord version for attachinary migration

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,11 @@
 class Cocktail < ApplicationRecord
+  # Model associations
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
+  # Attachinary file uploads
+  has_attachment :photo
+
+  # Model validations
   validates :name, presence: true, uniqueness: true
 end
